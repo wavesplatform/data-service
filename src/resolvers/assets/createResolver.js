4551363bb6a272b8fd74259fb6e81a11410b7fd1
Dmitry Shuranov <dvshur@gmail.com>
2018-04-20 00:32:53 +0300

    partially moved type signatures comments to jsdoc

--- src/resolvers/assets/createResolver.js
+++ src/resolvers/assets/createResolver.js
@@ -3,13 +3,13 @@ const Task = require('folktale/concurrency/task');
 const eitherToTask = require('../../utils/eitherToTask');
 const { compose, chain, map } = require('ramda');
 
-// query :: Options -> Task Asset[] AppError.Db
+/** query :: Options -> Task Asset[] AppError.Db */
 const query = ({ api, ids }) => api.assets(ids);
 
-// chainET :: (a -> Either b c) -> Task a d -> Task b c
+/** chainET :: (a -> Either b c) -> Task a d -> Task b c */
 const chainET = f => compose(chain(eitherToTask), map(f));
 
-// createAssetsResolver :: Dependencies -> Options -> Task Result AppError
+/** createAssetsResolver :: Dependencies -> Options -> Task Result AppError */
 const createAssetsResolver = ({ validateInput, validateResult }) =>
   compose(
     chainET(validateResult),
