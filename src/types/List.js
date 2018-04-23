d8db64d8ab8bc1054bb31ae41becf7f379679178
Dmitry Shuranov <dvshur@gmail.com>
2018-04-23 16:17:50 +0300

    added List type for responses with multiple objects

---
+++ src/types/List.js
@@ -0,0 +1,13 @@
+const { map, assoc } = require('ramda');
+
+const addListInfo = (items, cursorHashFn) => {
+  if (cursorHashFn) return map(d => assoc('cursor', cursorHashFn(d), d), items);
+  else return items;
+};
+
+const List = (items, cursorHashFn) => ({
+  __type: 'list',
+  items: addListInfo(items, cursorHashFn),
+});
+
+module.exports = List;
---
+++ src/types/List.test.js
@@ -0,0 +1,32 @@
+const List = require('./List');
+
+const { prop } = require('ramda');
+
+const items = [{ id: 'qwe', f: 0 }, { id: 'asd', f: 1 }];
+
+describe('List type should be', () => {
+  it('constructed from array', () => {
+    expect(List(items)).toEqual({
+      __type: 'list',
+      items,
+    });
+  });
+
+  it('apply a hash function', () => {
+    const hash = item => item.id + '_' + item.f;
+
+    expect(List(items, hash)).toEqual({
+      __type: 'list',
+      items: [
+        {
+          cursor: 'qwe_0',
+          ...items[0],
+        },
+        {
+          cursor: 'asd_1',
+          ...items[1],
+        },
+      ],
+    });
+  });
+});
