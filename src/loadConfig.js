cb5f162a93ce0cb9e5a3030d9219eeafd588c2d9
Dmitry Shuranov <dvshur@gmail.com>
2018-04-23 11:36:51 +0300

    loadConfig to memoize

--- src/loadConfig.js
+++ src/loadConfig.js
@@ -1,15 +1,21 @@
 const YAML = require('yamljs');
 const path = require('path');
 
-module.exports = () => {
-  let config = {};
+const { memoizeWith, always } = require('ramda');
+
+const loadConfig = () => {
+  let config;
+  console.log('Loading config');
   try {
     config = YAML.load(path.join(__dirname, '../config.yml'));
   } catch (err) {
     // eslint-disable-next-line
+    console.error(err);
     throw new Error(
       'Unable to read config file. Please add `config.yml` to root folder with necessary parameters'
     );
   }
   return config;
 };
+
+module.exports = memoizeWith(always('config'), loadConfig);
