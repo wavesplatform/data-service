3696daf00627d8ffb56f328a87ee6336883665d5
Dmitry Shuranov <dvshur@gmail.com>
2018-04-23 11:48:25 +0300

    db driver to memoize

--- src/db/driver/index.js
+++ src/db/driver/index.js
@@ -2,26 +2,21 @@
 const pgpConnect = require('./pgp');
 
 const { fromPromised } = require('folktale/concurrency/task');
-const { evolve, compose } = require('ramda');
+const { evolve, compose, memoizeWith, always } = require('ramda');
 
-let taskedDb;
-
-module.exports = (options, connect = pgpConnect) => {
+const createTaskedDriver = (options, connect = pgpConnect) => {
   const createDriver = compose(
     evolve({ many: fromPromised, none: fromPromised }),
     connect
   );
 
-  if (!taskedDb) {
-    // Evolve db driver to return Task
-    taskedDb = createDriver({
-      host: options.postgresHost,
-      port: options.postgresPort,
-      database: options.postgresDatabase,
-      user: options.postgresUser,
-      password: options.postgresPassword,
-    });
-  }
-
-  return taskedDb;
+  return createDriver({
+    host: options.postgresHost,
+    port: options.postgresPort,
+    database: options.postgresDatabase,
+    user: options.postgresUser,
+    password: options.postgresPassword,
+  });
 };
+
+module.exports = memoizeWith(always('taskedDb'), createTaskedDriver);
