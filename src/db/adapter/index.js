ce5e585b1f28c320582173c65d41ac3dcca9abe8
Dmitry Shuranov <dvshur@gmail.com>
2018-04-24 15:05:33 +0300

    moved Maybe to db adapter

--- src/db/adapter/index.js
+++ src/db/adapter/index.js
@@ -1,15 +1,20 @@
-const { propEq } = require('ramda');
+const { propEq, map } = require('ramda');
+
+const Maybe = require('folktale/maybe');
 
 // query and formatting
 const sql = require('../sql');
 // const { formatPairs } = require('../utils');
+
 // db adapter factory
 const createDbAdapter = ({ taskedDbDriver, batchQueryFn, errorFactory }) => {
   return {
+    /** assets :: AssetId[] -> Task (Maybe Result) AppError.Db */
     assets: assetIdArr =>
       taskedDbDriver
         .many(sql.assets, [assetIdArr])
         .map(batchQueryFn(propEq('asset_id'), assetIdArr))
+        .map(map(Maybe.fromNullable))
         .mapRejected(errorFactory({ request: 'assets' })),
 
     // volumes: pairs => db.many(sql.volumes, formatPairs(pairs)),
