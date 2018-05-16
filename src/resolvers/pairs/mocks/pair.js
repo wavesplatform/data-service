b19778822dc018d7951638c78321be73aa2579cd
Ivan Shmidt <bahoc47@gmail.com>
2018-05-16 20:05:31 +0300

    Updated pair mock for bignumber support

--- src/resolvers/pairs/mocks/pair.js
+++ src/resolvers/pairs/mocks/pair.js
@@ -1,12 +1,14 @@
+const { BigNumber } = require('@waves/data-entities');
+
 const pair = {
-  first_price: 0.00075632,
-  last_price: 0.00074128,
-  volume: 180232.92153489,
+  first_price: new BigNumber(0.00075632),
+  last_price: new BigNumber(0.00074128),
+  volume: new BigNumber(180232.92153489),
 };
 const transformedPair = {
-  firstPrice: 0.00075632,
-  lastPrice: 0.00074128,
-  volume: 180232.92153489,
+  firstPrice: new BigNumber(0.00075632),
+  lastPrice: new BigNumber(0.00074128),
+  volume: new BigNumber(180232.92153489),
 };
 
 module.exports = {
