9ebee1a6e5570f7e91a1c9c175307c5c0c37ba39
Ivan Shmidt <bahoc47@gmail.com>
2018-05-16 19:44:41 +0300

    Changed schema to support bigNumber

--- src/resolvers/pairs/validation/schema/common.js
+++ src/resolvers/pairs/validation/schema/common.js
@@ -1,21 +1,16 @@
 const Joi = require('joi');
-// const { BigNumber } = require('@waves/data-entities');
+const { BigNumber } = require('@waves/data-entities');
 
-// const output = Joi.object().keys({
-//   first_price: Joi.object()
-//     .type(BigNumber)
-//     .required(),
-//   last_price: Joi.object()
-//     .type(BigNumber)
-//     .required(),
-//   volume: Joi.object()
-//     .type(BigNumber)
-//     .required(),
-// });
 const output = Joi.object().keys({
-  first_price: Joi.number().required(),
-  last_price: Joi.number().required(),
-  volume: Joi.number().required(),
+  first_price: Joi.object()
+    .type(BigNumber)
+    .required(),
+  last_price: Joi.object()
+    .type(BigNumber)
+    .required(),
+  volume: Joi.object()
+    .type(BigNumber)
+    .required(),
 });
 
 const assetsIdRegex = /[0-9A-Za-z]+\/[0-9A-Za-z]+/;
