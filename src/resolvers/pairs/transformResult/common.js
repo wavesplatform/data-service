75e50807f6c5d8be3aac8e65f0c4dbb2ec506d51
Ivan Shmidt <bahoc47@gmail.com>
2018-05-16 19:45:46 +0300

    Changed transformation - deleted isNil check

--- src/resolvers/pairs/transformResult/common.js
+++ src/resolvers/pairs/transformResult/common.js
@@ -1,13 +1,12 @@
 const { renameKeys } = require('ramda-adjunct');
-const { compose, reject, isNil } = require('ramda');
+const { compose } = require('ramda');
 
 /** transformPairInfo:: RawPairInfo -> PairInfo */
 const transformPairInfo = compose(
   renameKeys({
     last_price: 'lastPrice',
     first_price: 'firstPrice',
-  }),
-  reject(isNil)
+  })
 );
 
 module.exports = { transformPairInfo };
