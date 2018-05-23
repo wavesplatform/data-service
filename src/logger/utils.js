535ab406db8935f77ce69b6c2a20307eff9bd78d
Ivan Shmidt <bahoc47@gmail.com>
2018-05-23 16:56:44 +0300

    Empowered logs in dev mode

---
+++ src/logger/utils.js
@@ -0,0 +1,22 @@
+const { identity, pathEq, ifElse } = require('ramda');
+var colorize = require('json-colorizer');
+
+const isDev = pathEq(['env', 'NODE_ENV'], 'development');
+const stringifyInProd = ifElse(
+  isDev,
+  () => identity,
+  () => JSON.stringify.bind(JSON)
+)(process);
+const separator = () => new Array(64).fill('-').join('');
+
+const stringify = ifElse(
+  isDev,
+  () => json => `${colorize(JSON.stringify(json, null, 2))}\n${separator()}`,
+  () => json => JSON.stringify(json)
+)(process);
+
+module.exports = {
+  isDev,
+  stringifyInProd,
+  stringify,
+};
