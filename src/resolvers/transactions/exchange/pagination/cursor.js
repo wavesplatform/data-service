f15f237f1ba28e90b4ed16d59638eaae77ff98f5
Ivan Shmidt <bahoc47@gmail.com>
2018-06-07 11:35:19 +0300

    Added base64 encoding/decoding for cursor

--- src/resolvers/transactions/exchange/pagination/cursor.js
+++ src/resolvers/transactions/exchange/pagination/cursor.js
@@ -2,10 +2,15 @@ const { last, head } = require('ramda');
 
 const cursorHashFn = request => items => {
   const item = (request.sort === 'desc' ? last(items) : head(items)).data;
-  return `${item.timestamp.toISOString()}::${item.id}::${request.sort}`;
+  return Buffer.from(
+    `${item.timestamp.toISOString()}::${item.id}::${request.sort}`
+  ).toString('base64');
 };
 
-const decode = cursor => cursor.split('::');
+const decode = cursor =>
+  Buffer.from(cursor, 'base64')
+    .toString('ascii')
+    .split('::');
 
 module.exports = {
   cursorHashFn,
