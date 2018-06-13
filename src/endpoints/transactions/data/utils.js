4794dd4a9e1a381b0c82ce42dd7bd7a4c1af8f38
Dmitry Shuranov <dvshur@gmail.com>
2018-06-13 17:58:46 +0300

    added endpoint under env flag

---
+++ src/endpoints/transactions/data/utils.js
@@ -0,0 +1,27 @@
+const { reject, isNil } = require('ramda');
+const { parseDate } = require('../../../utils/parseDate');
+
+const selectFilters = ({
+  timeStart, // No default value for timestart, other way - bad for desc pagination
+  timeEnd,
+  sender,
+  limit = 100,
+  sort = 'desc',
+  key,
+  type,
+  value,
+  // after,
+}) =>
+  reject(isNil, {
+    timeStart: timeStart && parseDate(timeStart),
+    timeEnd: timeEnd && parseDate(timeEnd),
+    limit,
+    sort,
+    sender,
+    key,
+    type,
+    value,
+    // after,
+  });
+
+module.exports = { selectFilters };
--- src/endpoints/transactions/index.js
+++ src/endpoints/transactions/index.js
@@ -5,7 +5,15 @@ const subrouter = new Router();
 const exchangeOne = require('./exchange/one');
 const exchangeMany = require('./exchange/many');
 
+const dataOne = require('./data/one');
+const dataMany = require('./data/many');
+
 subrouter.get('/transactions/exchange/:id', exchangeOne);
 subrouter.get('/transactions/exchange', exchangeMany);
+
+if (process.env.API_DATA_TXS_ENABLED) {
+  subrouter.get('/transactions/data/:id', dataOne);
+  subrouter.get('/transactions/data', dataMany);
+}
 
 module.exports = subrouter;
