51478cbe83340bf53196883e505e2c2ad8abc78c
Ivan Shmidt <bahoc47@gmail.com>
2018-06-20 15:20:50 +0300

    Added pagination for data txs

--- src/db/sql/build/transactions/exchange/filters.js
+++ src/db/sql/build/transactions/exchange/filters.js
@@ -13,17 +13,24 @@ const bySender = curryN(2, (sender, q) =>
 
 const after = ({ timestamp, id, sortDirection }) => q => {
   const comparator = sortDirection === 'desc' ? '<' : '>';
-  return q.whereRaw(`(t.time_stamp, t.id) ${comparator} (?, ?)`, [
-    timestamp,
-    id,
-  ]);
+  return q
+    .clone()
+    .whereRaw(`(t.time_stamp, t.id) ${comparator} (?, ?)`, [timestamp, id]);
 };
 
 module.exports = {
   id: where('t.id'),
   after,
-  sortOuter: s => q => q.orderBy('tx_time_stamp', s).orderBy('tx_id', s),
-  sort: s => q => q.orderBy('t.time_stamp', s).orderBy('t.id', s),
+  sortOuter: s => q =>
+    q
+      .clone()
+      .orderBy('tx_time_stamp', s)
+      .orderBy('tx_id', s),
+  sort: s => q =>
+    q
+      .clone()
+      .orderBy('t.time_stamp', s)
+      .orderBy('t.id', s),
   matcher: where('t.sender'),
   amountAsset: where('t.amount_asset'),
   priceAsset: where('t.price_asset'),
