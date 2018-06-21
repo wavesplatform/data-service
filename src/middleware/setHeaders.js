710d46d6e0617493ec670e8cf425df45e5a11896
Ivan Shmidt <bahoc47@gmail.com>
2018-06-21 16:26:52 +0300

    Added X-REQUEST-ID header

--- src/middleware/setHeaders.js
+++ src/middleware/setHeaders.js
@@ -3,6 +3,8 @@ const compose = require('koa-compose');
 
 const setHeaders = async (ctx, next) => {
   ctx.set('Content-Type', 'application/json; charset=utf-8');
+  ctx.set('X-REQUEST-ID', ctx.state.id);
+
   await next();
 };
 
