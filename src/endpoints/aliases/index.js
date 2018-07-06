fbc61a43dbf82ad57fe77f485e8d5eefa7a2affb
Ivan Shmidt <bahoc47@gmail.com>
2018-07-06 16:31:09 +0300

    Added POST support for 'many' endpoints

--- src/endpoints/aliases/index.js
+++ src/endpoints/aliases/index.js
@@ -1,8 +1,10 @@
 const Router = require('koa-router');
 
 const subrouter = new Router();
+const postToGet = require('../utils/postToGet');
 
 subrouter.get('/aliases/:alias', require('./one'));
 subrouter.get('/aliases', require('./many'));
+subrouter.post('/aliases', postToGet(require('./many')));
 
 module.exports = subrouter;
