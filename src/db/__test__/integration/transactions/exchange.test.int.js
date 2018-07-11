00d626da45ff7a48e7509825c96aba7be13cd29b
Dmitry Shuranov <dvshur@gmail.com>
2018-07-11 15:01:12 +0300

    increased MIN_LIMIT for exchange txs (performance hack)

--- src/db/__test__/integration/transactions/exchange.test.int.js
+++ src/db/__test__/integration/transactions/exchange.test.int.js
@@ -3,6 +3,10 @@ const { Nothing } = require('folktale/maybe');
 const loadConfig = require('../../../../loadConfig');
 const createDb = require('../../../index');
 
+const {
+  MIN_LIMIT,
+} = require('../../../adapter/transactions/exchange/constants');
+
 const db = createDb(loadConfig());
 
 describe('Exchange transactions should return ', () => {
@@ -44,7 +48,7 @@ describe('Exchange transactions should return ', () => {
     });
 
     describe('limit filter', () => {
-      it('should work with limit 1 (less than MIN_LIMIT of 5)', done => {
+      it('should work with limit 1 (less than MIN_LIMIT)', done => {
         db.transactions.exchange
           .many({ limit: 1 })
           .run()
@@ -57,14 +61,14 @@ describe('Exchange transactions should return ', () => {
           });
       });
 
-      it('should work with limit 10 (more than MIN_LIMIT of 5)', done => {
+      it('should work with limit 10 (more than MIN_LIMIT)', done => {
         db.transactions.exchange
-          .many({ limit: 10 })
+          .many({ limit: MIN_LIMIT + 1 })
           .run()
           .listen({
             onResolved: mxs => {
               const xs = mxs.map(x => x.getOrElse(-1));
-              expect(xs.length).toBe(10);
+              expect(xs.length).toBe(MIN_LIMIT + 1);
               done();
             },
           });
