596cc181cd6b7692779c3244416fcf75e2d68cf3
Dmitry Shuranov <dvshur@gmail.com>
2018-08-03 16:11:14 +0300

    parametrized docsUrl via env, removed productionUrl

--- src/endpoints/root.js
+++ src/endpoints/root.js
@@ -4,12 +4,15 @@ module.exports = async ctx => {
   ctx.eventBus.emit('ENDPOINT_HIT', {
     url: ctx.originalUrl,
   });
+
   ctx.state.returnValue = {
     version,
-    productionUrl: 'https://api.wavesplatform.com/v0',
-    docsUrl: 'https://api.wavesplatform.com/v0/docs',
     github: 'https://github.com/wavesplatform/data-service',
   };
+
+  if (process.env.DOCS_URL)
+    ctx.state.returnValue.docsUrl = process.env.DOCS_URL;
+
   ctx.eventBus.emit('ENDPOINT_RESOLVED', {
     value: ctx.state.returnValue,
   });
