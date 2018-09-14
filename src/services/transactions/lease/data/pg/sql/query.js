c321c2717f209ab14d061cfb1a862b39487c00cd
Dmitry Shuranov <dvshur@gmail.com>
2018-09-14 13:36:16 +0300

    added leasing txs API (#90)

---
+++ src/services/transactions/lease/data/pg/sql/query.js
@@ -0,0 +1,18 @@
+const pg = require('knex')({ client: 'pg' });
+
+const select = pg({ t: 'txs_8' }).select({
+  height: 't.height',
+  tx_type: 't.tx_type',
+  id: 't.id',
+  time_stamp: 't.time_stamp',
+  signature: 't.signature',
+  proofs: 't.proofs',
+  tx_version: 't.tx_version',
+  fee: pg.raw('t.fee * 10^(-8)'),
+  sender: 't.sender',
+  sender_public_key: 't.sender_public_key',
+  amount: pg.raw('t.amount * 10^(-8)'),
+  recipient: 't.recipient',
+});
+
+module.exports = select;
