9ddb64af3981a4384e6503743c83a7c8cae44924
Dmitry Shuranov <dvshur@gmail.com>
2018-09-26 16:46:32 +0300

    added alias get/mget service

---
+++ src/services/transactions/alias/schema.js
@@ -0,0 +1,31 @@
+const Joi = require('../../../utils/validation/joi');
+
+const result = Joi.object().keys({
+  id: Joi.string()
+    .base58()
+    .required(),
+  height: Joi.number().required(),
+  tx_type: Joi.number().required(),
+  tx_version: Joi.number()
+    .required()
+    .allow(null),
+  fee: Joi.object()
+    .bignumber()
+    .required(),
+  time_stamp: Joi.date().required(),
+  sender: Joi.string().required(),
+  sender_public_key: Joi.string().required(),
+
+  signature: Joi.string()
+    .required()
+    .allow(''),
+  proofs: Joi.when('signature', {
+    is: '',
+    then: Joi.array().min(1),
+    otherwise: Joi.array().length(0),
+  }).required(),
+
+  alias: Joi.string().required(),
+});
+
+module.exports = { result };
