7913e4cd5aefd5bffdf2e94eb562bbc5f6a0f2b5
Dmitry Shuranov <dvshur@gmail.com>
2018-09-26 18:03:42 +0300

    data txs proofs fix

--- src/services/transactions/data/transformTxInfo.js
+++ src/services/transactions/data/transformTxInfo.js
@@ -3,6 +3,6 @@ const { compose, reject, isNil } = require('ramda');
 const transformTxInfo = require('../common/transformTxInfo');
 
 module.exports = compose(
-  transformTxInfo,
-  reject(isNil)
+  reject(isNil),
+  transformTxInfo
 );
