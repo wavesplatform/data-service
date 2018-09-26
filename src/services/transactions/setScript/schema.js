b785a9d8482f08fd5148c1362e234dfaa4c46dcf
Dmitry Shuranov <dvshur@gmail.com>
2018-09-26 17:31:00 +0300

    setScript service added

---
+++ src/services/transactions/setScript/schema.js
@@ -0,0 +1,33 @@
+const Joi = require('../../../utils/validation/joi');
+
+const result = Joi.object().keys({
+  id: Joi.string()
+    .base58()
+    .required(),
+  height: Joi.number().required(),
+  tx_type: Joi.number().required(),
+  tx_version: Joi.number()
+    .required()
+    .allow(null),
+  fee: Joi.object()
+    .bignumber()
+    .required(),
+  time_stamp: Joi.date().required(),
+  sender: Joi.string().required(),
+  sender_public_key: Joi.string().required(),
+
+  signature: Joi.string()
+    .required()
+    .allow(''),
+  proofs: Joi.when('signature', {
+    is: '',
+    then: Joi.array().min(1),
+    otherwise: Joi.array().length(0),
+  }).required(),
+
+  script: Joi.string()
+    .required()
+    .allow(null),
+});
+
+module.exports = { result };
