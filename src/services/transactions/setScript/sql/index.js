a136b650d23f9cdfa8be8e9eaaad8da8c903d39f
Dmitry Shuranov <dvshur@gmail.com>
2018-09-27 13:36:17 +0300

    added filters to sql

--- src/services/transactions/setScript/sql/index.js
+++ src/services/transactions/setScript/sql/index.js
@@ -1,4 +1,4 @@
-const { pipe } = require('ramda');
+const { pipe, compose, map, pick, filter, has, __ } = require('ramda');
 
 const F = require('./filters');
 const { select } = require('./query');
@@ -15,4 +15,29 @@ module.exports = {
       F.ids(ids),
       String
     )(select),
+
+  search: fValues => {
+    const order = [
+      'id',
+      'sender',
+      'script',
+      'after',
+      'timeStart',
+      'timeEnd',
+      'sort',
+      'limit',
+    ];
+
+    const fValuesPicked = pick(order, fValues);
+
+    const appliedFs = compose(
+      map(x => F[x](fValuesPicked[x])),
+      filter(has(__, fValuesPicked))
+    )(order);
+    return pipe(
+      q => q.clone(),
+      ...appliedFs,
+      String
+    )(select);
+  },
 };
