fc35f8b5cb31a7946eba5269be3256246e1aac37
Dmitry Shuranov <dvshur@gmail.com>
2018-10-08 16:13:00 +0300

    all txs commonData sql uses common sql build

--- src/services/transactions/all/commonData/sql/index.js
+++ src/services/transactions/all/commonData/sql/index.js
@@ -1,42 +1,5 @@
-const { pipe, compose, map, pick, filter, has, __ } = require('ramda');
+const createSql = require('../../../_common/sql');
 
-const F = require('./filters');
-const commonQuery = require('./query');
+const { select } = require('./query');
 
-module.exports = {
-  get: id =>
-    pipe(
-      F.id(id),
-      String
-    )(commonQuery),
-
-  mget: ids =>
-    pipe(
-      F.ids(ids),
-      String
-    )(commonQuery),
-
-  search: fValues => {
-    const order = [
-      'id',
-      'sender',
-      'after',
-      'timeStart',
-      'timeEnd',
-      'sort',
-      'limit',
-    ];
-
-    const fValuesPicked = pick(order, fValues);
-
-    const appliedFs = compose(
-      map(x => F[x](fValuesPicked[x])),
-      filter(has(__, fValuesPicked))
-    )(order);
-    return pipe(
-      q => q.clone(),
-      ...appliedFs,
-      String
-    )(commonQuery);
-  },
-};
+module.exports = createSql({ query: select });
