9d6a9357c15861e7065edb7ec1d08e6442826bde
Kozhevnikov Peter <kozhevnikov.peter@gmail.com>
2018-12-04 17:44:28 +0300

    Fix daemon -> daemons + fix config load file

---
+++ src/daemons/candles/create.js
@@ -0,0 +1,133 @@
+const { head } = require('ramda');
+const Task = require('folktale/concurrency/task');
+
+const logTaskProgress = require('../utils/logTaskProgress');
+
+const {
+  truncateTable,
+  insertAllMinuteCandles,
+  insertAllCandles,
+  selectCandlesByMinute,
+  insertOrUpdateCandles,
+  selectLastCandle,
+  selectLastExchangeTx,
+  insertOrUpdateCandlesFromHeight,
+} = require('./sql/query');
+
+/** for combining candles */
+const intervalPairs = [
+  [60, 300],
+  [300, 900],
+  [900, 1800],
+  [1800, 3600],
+  [3600, 86400],
+];
+
+/** getStartBlock :: (Object, Object) -> Number */
+const getStartBlock = (exchangeTx, candle) => {
+  if (candle && exchangeTx) {
+    if (candle.max_height > exchangeTx.height) {
+      return exchangeTx.height - 2000; // handle rollback
+    } else {
+      return Math.min(candle.max_height, exchangeTx.height) - 2;
+    }
+  }
+
+  return 1;
+};
+
+/** updateCandlesLoop :: (LogTask, Pg, String) -> Task */
+const updateCandlesLoop = (logTask, pg, tableName) => {
+  const logMessages = {
+    start: timeStart => ({
+      message: '[CANDLES] start updating candles',
+      time: timeStart,
+    }),
+    error: (e, timeTaken) => ({
+      message: '[CANDLES] update error',
+      time: timeTaken,
+      error: e,
+    }),
+    success: (_, timeTaken) => ({
+      message: '[CANDLES] update successful',
+      time: timeTaken,
+    }),
+  };
+
+  const pgPromiseUpdateCandles = (t, startBlockHeight) =>
+    t
+      .any(selectCandlesByMinute(startBlockHeight))
+      .then(candles => t.any(insertOrUpdateCandles(tableName, candles)));
+
+  return logTask(
+    logMessages,
+    pg.tx(t =>
+      t
+        .batch([
+          t.any(selectLastExchangeTx()),
+          t.any(selectLastCandle(tableName)),
+        ])
+        .then(([lastTx, candle]) => {
+          const startHeight = getStartBlock(head(lastTx), head(candle));
+          return pgPromiseUpdateCandles(t, startHeight).then(() =>
+            t.batch(
+              intervalPairs.map(([shorter, longer]) =>
+                t.any(insertOrUpdateCandlesFromHeight(tableName, shorter, longer, startHeight))
+              )
+            )
+          );
+        })
+    )
+  );
+};
+
+/** fillCandlesDBAll :: (LogTask, Pg, String) -> Task */
+const fillCandlesDBAll = (logTask, pg, tableName) =>
+  logTask(
+    {
+      start: timeStart => ({
+        message: '[DB] start filling',
+        time: timeStart,
+      }),
+      error: (e, timeTaken) => ({
+        message: '[DB] fill error',
+        time: timeTaken,
+        error: e,
+      }),
+      success: (_, timeTaken) => ({
+        message: '[DB] fill successful',
+        time: timeTaken,
+      }),
+    },
+    pg.tx(t =>
+      t.batch([
+        t.any(truncateTable(tableName)),
+        t.any(insertAllMinuteCandles(tableName)),
+        ...intervalPairs.map(([shorter, longer]) =>
+          t.any(insertAllCandles(tableName, shorter, longer))
+        ),
+      ])
+    )
+  );
+
+module.exports = ({ logger, pg }, configuration) => {
+  const unsafeLogTaskProgress = logTaskProgress(logger);
+
+  return {
+    init: () => {
+      if (configuration.candlesTruncateTable)
+        return fillCandlesDBAll(
+          unsafeLogTaskProgress,
+          pg,
+          configuration.candlesTableName
+        );
+      else return Task.of();
+    },
+    loop: () =>
+      updateCandlesLoop(
+        unsafeLogTaskProgress,
+        pg,
+        configuration.candlesTableName
+      ),
+  };
+};
---
+++ src/daemons/candles/sql/__test__/sql.test.js
@@ -0,0 +1,58 @@
+const sql = require('../query');
+const { BigNumber } = require('@waves/data-entities');
+
+describe('candles daemon sql test', () => {
+  it('truncate table', () => {
+    expect(sql.truncateTable('candles')).toMatchSnapshot();
+  });
+
+  it('insert all candles group by 1 minute', () => {
+    expect(sql.insertAllMinuteCandles('candles')).toMatchSnapshot();
+  });
+
+  it('calculate and insert all candles from other small candles', () => {
+    expect(sql.insertAllCandles('candles', 60, 300)).toMatchSnapshot();
+  });
+
+  it('get all candles from exchange tx grouped by 1 minute and after 122 block height', () => {
+    expect(sql.selectCandlesByMinute(122)).toMatchSnapshot();
+  });
+
+  it('insert or update array of candles', () => {
+    expect(
+      sql
+        .insertOrUpdateCandles('candles', [
+          {
+            time_start: new Date(0),
+            low: new BigNumber(1),
+            high: new BigNumber(100),
+            open: new BigNumber(20),
+            close: new BigNumber(80),
+            amount_asset_id: '1',
+            price_asset_id: '2',
+            price: new BigNumber(1.2),
+            volume: new BigNumber(200.2),
+            price_volume: new BigNumber(100.2),
+            txs_count: new BigNumber(22),
+            weighted_average_price: new BigNumber(2.1),
+          },
+        ])
+    ).toMatchSnapshot();
+  });
+
+  it('insert or update candles empty', () => {
+    expect(sql.insertOrUpdateCandles('candles', []).toString()).toMatchSnapshot();
+  });
+
+  it('get last candle', () => {
+    expect(sql.selectLastCandle('candles').toString()).toMatchSnapshot();
+  });
+
+  it('get last exchange tx', () => {
+    expect(sql.selectLastExchangeTx('candles').toString()).toMatchSnapshot();
+  });
+
+  it('insert or update candles from height', () => {
+    expect(sql.insertOrUpdateCandlesFromHeight('candles', 60, 300, 211).toString()).toMatchSnapshot();
+  });
+});
