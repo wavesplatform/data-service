6282d46e693f741c61f6013831405129fe4b0e24
apetrov <apetrov@wv00293.local>
2018-12-06 14:52:20 +0300

    refactor sql script

--- src/daemons/pairs/sql/fillTable.js
+++ src/daemons/pairs/sql/fillTable.js
@@ -1,8 +1,8 @@
 const knex = require('knex');
 const pg = knex({ client: 'pg' });
 
-const selectVolumeWavesFromPairs = pg({ d: 'pairs' })
-  .columns([pg.raw('p.volume * d.weighted_average_price')])
+const selectVolumeWavesFromTempPairs = pg({ d: 'temp_pairs' })
+  .select({ volume_waves: pg.raw('p.volume * d.weighted_average_price') })
   .whereRaw(`(d.amount_asset_id=p.price_asset_id AND d.price_asset_id='WAVES')`)
   .orWhereRaw(
     `(d.price_asset_id=p.price_asset_id AND d.amount_asset_id='WAVES')`
@@ -13,38 +13,28 @@ const selectExchanges = pg('txs_7')
   .whereRaw(`time_stamp >= now() - interval '1 day'`)
   .orderBy('time_stamp', 'desc');
 
-const selectPairs = pg
-  .with('pairs', qp => {
-    qp.columns(
-      { amount_asset_id: 'amount_asset' },
-      { price_asset_id: 'price_asset' },
-      {
-        last_price: pg.raw(
-          '(array_agg(e.price * 10 ^(-8 - p_dec.decimals + a_dec.decimals))::numeric[])[1]'
-        ),
-      },
-      {
-        first_price: pg.raw(
-          '(array_agg(e.price * 10 ^(-8 - p_dec.decimals + a_dec.decimals))::numeric[])[array_length(array_agg(e.price * 10 ^(-8 - p_dec.decimals + a_dec.decimals))::numeric[], 1)]'
-        ),
-      },
-      { volume: pg.raw('sum(e.amount * 10 ^(-a_dec.decimals))') },
-      {
-        volume_price: pg.raw(
-          'sum(e.amount * 10 ^(-a_dec.decimals) * e.price * 10 ^(-8 - p_dec.decimals + a_dec.decimals))'
-        ),
-      },
-      {
-        weighted_average_price: pg.raw(
-          'sum(e.amount * 10 ^(-a_dec.decimals) * e.price * 10 ^(-8 - p_dec.decimals + a_dec.decimals))/ sum(e.amount * 10 ^(-a_dec.decimals))'
-        ),
-      },
-      {
-        volume_waves: pg.raw(
-          "case when amount_asset = 'WAVES' then sum(e.amount * 10 ^(-a_dec.decimals)) when price_asset = 'WAVES' then sum(e.amount * 10 ^(-a_dec.decimals) * e.price * 10 ^(-8 - p_dec.decimals + a_dec.decimals)) end"
-        ),
-      }
-    )
+const selectTempPairs = pg
+  .with('temp_pairs', qp => {
+    qp.columns({
+      amount_asset_id: 'amount_asset',
+      price_asset_id: 'price_asset',
+      last_price: pg.raw(
+        '(array_agg(e.price * 10 ^(-8 - p_dec.decimals + a_dec.decimals))::numeric[])[1]'
+      ),
+      first_price: pg.raw(
+        '(array_agg(e.price * 10 ^(-8 - p_dec.decimals + a_dec.decimals))::numeric[])[array_length(array_agg(e.price * 10 ^(-8 - p_dec.decimals + a_dec.decimals))::numeric[], 1)]'
+      ),
+      volume: pg.raw('sum(e.amount * 10 ^(-a_dec.decimals))'),
+      volume_price: pg.raw(
+        'sum(e.amount * 10 ^(-a_dec.decimals) * e.price * 10 ^(-8 - p_dec.decimals + a_dec.decimals))'
+      ),
+      weighted_average_price: pg.raw(
+        'sum(e.amount * 10 ^(-a_dec.decimals) * e.price * 10 ^(-8 - p_dec.decimals + a_dec.decimals))/ sum(e.amount * 10 ^(-a_dec.decimals))'
+      ),
+      volume_waves: pg.raw(
+        "case when amount_asset = 'WAVES' then sum(e.amount * 10 ^(-a_dec.decimals)) when price_asset = 'WAVES' then sum(e.amount * 10 ^(-a_dec.decimals) * e.price * 10 ^(-8 - p_dec.decimals + a_dec.decimals)) end"
+      ),
+    })
       .from(selectExchanges.clone().as('e'))
       .innerJoin(
         { a_dec: 'asset_decimals' },
@@ -54,7 +44,7 @@ const selectPairs = pg
       .innerJoin({ p_dec: 'asset_decimals' }, 'e.price_asset', 'p_dec.asset_id')
       .groupBy(['amount_asset', 'price_asset']);
   })
-  .from({ p: 'pairs' })
+  .from({ p: 'temp_pairs' })
   .columns(
     'amount_asset_id',
     'price_asset_id',
@@ -63,13 +53,18 @@ const selectPairs = pg
     'volume',
     {
       volume_waves: pg.raw(
-        `coalesce(volume_waves, (${selectVolumeWavesFromPairs.toString()}))`
+        `coalesce(volume_waves, (${selectVolumeWavesFromTempPairs.toString()}))`
       ),
     }
   );
-
-module.exports = tableName =>
+console.log(
   pg
-    .into(tableName)
-    .insert(selectPairs)
-    .toString();
+    .into('pairs')
+    .insert(selectTempPairs)
+    .toString()
+);
+// module.exports = tableName =>
+//   pg
+//     .into(tableName)
+//     .insert(selectTempPairs)
+//     .toString();
