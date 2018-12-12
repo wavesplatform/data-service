45b11e8c803eb7713dc91441bd7c348fcd0f3baf
Kozhevnikov Peter <kozhevnikov@myriad-corp.com>
2018-12-12 17:26:54 +0300

    DATA-218 search pairs (#134)
    * search realization
    
    * Fix filterParsers
    
    * Fix import
    
    * Fix return Object to (Object)

--- src/services/pairs/__test__/transformResults.test.js
+++ src/services/pairs/__test__/transformResults.test.js
@@ -1,7 +1,11 @@
-const transformResult = require('../transformResult');
+const {
+  transformResult,
+  transformResultSearch,
+} = require('../transformResult');
+const { List, Pair } = require('../../../types');
 
 describe('sql query results transformation', () => {
-  it('pair without asset and price asset', () => {
+  it('should return pair without asset and price asset for get one or many pair', () => {
     expect(
       transformResult({
         amount_asset_id: '111',
@@ -17,5 +21,33 @@ describe('sql query results transformation', () => {
       volume: 100.1,
       volumeWaves: 10.2,
     });
+  });
+
+  it('should return pair with asset and price asset for search pairs', () => {
+    expect(
+      transformResultSearch([
+        {
+          amount_asset_id: '111',
+          price_asset_id: '222',
+          first_price: 1.2,
+          last_price: 2.1,
+          volume: 100.1,
+          volume_waves: 10.2,
+        },
+      ])
+    ).toEqual(
+      List([
+        {
+          ...Pair({
+            firstPrice: 1.2,
+            lastPrice: 2.1,
+            volume: 100.1,
+            volumeWaves: 10.2,
+          }),
+          amountAsset: '111',
+          priceAsset: '222',
+        },
+      ])
+    );
   });
 });
