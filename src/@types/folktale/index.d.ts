35c0d9d8876507084558ca85e8e220a9ab066230
Dmitry Shuranov <dvshur@gmail.com>
2018-12-13 17:55:55 +0300

    folktale task types added

--- src/@types/folktale/index.d.ts
+++ src/@types/folktale/index.d.ts
@@ -3,45 +3,102 @@ declare module 'folktale/concurrency/task' {
     matchWith<C>(pattern: Record<string, (...args: any[]) => C>): C;
   }
 
-  type TaskPattern<A, B, C> = {
+  export type TaskPattern<A, B, C> = {
     Cancelled: () => C;
-    Resolved: (b: B) => C;
     Rejected: (a: A) => C;
+    Resolved: (b: B) => C;
   };
 
-  abstract class Task<A, B> implements Matchable {
-    protected constructor();
-    public static of<A, B>(value: B): Task<A, B>;
-    public abstract matchWith<C>(pattern: TaskPattern<A, B, C>): C;
+  export interface TaskExecution<A, B> {
+    cancel(): void;
+    promise(): Promise<B>;
 
-    // task methods
-    public map<C>(f: (b: B) => C): Task<A, C>;
-    public chain<C>(f: (b: B) => Task<A, C>): Task<A, C>;
-    public mapRejected<C>(f: (a: A) => C): Task<C, B>;
+    listen(handlers: {
+      onCancelled?: () => void;
+      onRejected?: (a: A) => void;
+      onResolved?: (b: B) => void;
+    }): void;
+
+    link<C, D>(other: TaskExecution<C, D>): TaskExecution<C, D>;
+
+    c: ThisType<number>;
+  }
+
+  export type Computation<A, B> = (
+    resolver: {
+      reject: (reason: A) => void;
+      resolve: (value: B) => void;
+      cancel: () => void;
+      cleanup: (cb: () => void) => void;
+      onCancelled: (cb: () => void) => void;
+      readonly isCancelled: boolean;
+    }
+  ) => void;
+
+  interface TaskConstructor {
+    new <A, B>(computation: Computation<A, B>): Task<A, B>;
+    of<A, B>(value: B): Task<A, B>;
+    rejected<A, B>(reason: B): Task<A, B>;
+  }
+
+  export interface Task<A, B> extends Matchable {
+    // pattern matching
+    // general
+    matchWith<C>(pattern: TaskPattern<A, B, C>): C;
+    // type-specific
+    willMatchWith<C, D>(pattern: TaskPattern<A, B, Task<C, D>>): Task<C, D>;
+
+    // combining tasks
+    // and<C, D>(t: Task<C, D>): Task<A | C, [B, D]>;
+    and<C, D>(t: Task<C, D>): Task<A | C, [B, D]>;
+    or<C, D>(t: Task<C, D>): Task<A | C, B | D>;
+
+    map<C>(f: (b: B) => C): Task<A, C>;
+    chain<C>(f: (b: B) => Task<A, C>): Task<A, C>;
+    mapRejected<C>(f: (a: A) => C): Task<C, B>;
     // checks if the current task contains an appropriate function
-    public apply<C, D>(
+    // maybe should be `never` instead
+    apply<C, D>(
       f: Task<C, D>
-    ): B extends (d: D) => any ? Task<C, ReturnType<B>> : unknown;
+    ): B extends (d: D) => infer R ? Task<C, R> : unknown;
+    bimap<C, D>(lf: (a: A) => C, rf: (b: B) => D): Task<C, D>;
+
+    orElse<C, D>(f: (a: A) => Task<C, D>): Task<C, D>;
+    swap(): Task<B, A>;
+
+    run(): TaskExecution<A, B>;
   }
 
-  class Cancelled<A, B> extends Task<A, B> {
-    // constructor();
-    public matchWith<C>(pattern: TaskPattern<A, B, C>): C;
-  }
+  export const of: TaskConstructor['of'];
+  export const rejected: TaskConstructor['rejected'];
+  export const task: <A, B>(computation: Computation<A, B>) => Task<A, B>;
 
-  class Resolved<A, B> extends Task<A, B> {
-    constructor(value: B);
-    public matchWith<C>(pattern: TaskPattern<A, B, C>): C;
-  }
+  // one error parameter
+  function waitAny<A1, B1>(ts: [Task<A1, B1>]): Task<A1, [B1]>;
+  function waitAny<A1, B1, A2, B2>(
+    ts: [Task<A1, B1>, Task<A2, B2>]
+  ): Task<A1 | A2, B1 | B2>;
+  function waitAny<A1, B1, A2, B2, A3, B3>(
+    ts: [Task<A1, B1>, Task<A2, B2>, Task<A3, B3>]
+  ): Task<A1 | A2 | A3, B1 | B2 | B3>;
+  function waitAny<A1, B1, A2, B2, A3, B3, A4, B4>(
+    ts: [Task<A1, B1>, Task<A2, B2>, Task<A3, B3>, Task<A4, B4>]
+  ): Task<A1 | A2 | A3 | A4, B1 | B2 | B3 | B4>;
+  export function waitAny<E, V>(ts: Task<E, V>[]): Task<E, V[]>;
 
-  class Rejected<A, B> extends Task<A, B> {
-    constructor(value: B);
-    public matchWith<C>(pattern: TaskPattern<A, B, C>): C;
-  }
+  // all errors parameters
+  function waitAll<A1, B1>(ts: [Task<A1, B1>]): Task<A1, [B1]>;
+  function waitAll<A1, B1, A2, B2>(
+    ts: [Task<A1, B1>, Task<A2, B2>]
+  ): Task<A1 | A2, [B1, B2]>;
+  function waitAll<A1, B1, A2, B2, A3, B3>(
+    ts: [Task<A1, B1>, Task<A2, B2>, Task<A3, B3>]
+  ): Task<A1 | A2 | A3, [B1, B2, B3]>;
+  function waitAll<A1, B1, A2, B2, A3, B3, A4, B4>(
+    ts: [Task<A1, B1>, Task<A2, B2>, Task<A3, B3>, Task<A4, B4>]
+  ): Task<A1 | A2 | A3 | A4, [B1, B2, B3, B4]>;
+  export function waitAll<E, V>(ts: Task<E, V>[]): Task<E, V[]>;
 
-  interface TaskExecution {}
-
-  interface Task<A, B> {
-    map<C>(f: (a: A) => C): Task<A, C>;
-  }
+  // @todo
+  // function do(...)
 }
