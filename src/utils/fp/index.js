96429e9d0a441dda0a495a069f1434f930fd7a9e
Dmitry Shuranov <dvshur@gmail.com>
2018-12-18 15:28:21 +0300

    tests for fp utils

--- src/utils/fp/index.js
+++ src/utils/fp/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   resultToTask: require('./resultToTask'),
-  swapMaybeF: require('./swapMaybeF'),
-  liftInnerMaybe: require('./liftInnerMaybe'),
+  swapMaybeF: require('./swapMaybeF').swapMaybeF,
+  liftInnerMaybe: require('./liftInnerMaybe').liftInnerMaybe,
   concatAll: require('./concatAll'),
 };
