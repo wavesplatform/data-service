49dd6aba0ee03fbc8d18d69a095372398ef164bd
Dmitry Shuranov <dvshur@gmail.com>
2019-01-22 19:13:48 +0300

    asset sql fields instead of wildcard

--- src/services/assets/data/sql.js
+++ src/services/assets/data/sql.js
@@ -2,6 +2,17 @@ const pg = require('knex')({ client: 'pg' });
 
 module.exports = ids =>
   pg('assets')
-    .select('*')
+    .select([
+      'asset_id',
+      'asset_name',
+      'description',
+      'sender',
+      'issue_height',
+      'total_quantity',
+      'decimals',
+      'reissuable',
+      'ticker',
+      'issue_timestamp'
+    ])
     .whereIn('asset_id', ids)
     .toString();
