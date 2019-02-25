10fb24153452dc312bf0aca528841ede62ba6026
kozhevnikov-peter <pkozhevnikov@wavesplatform.com>
2019-02-25 13:37:11 +0300

    Fix

--- src/types/__test__/monoid.test.ts
+++
@@ -1,8 +0,0 @@
-describe('Monoid type should be', () => {
-  it('constructed from object', () => {
-    expect({
-      concat: (a: number, b: number) => a + b,
-      empty: 0,
-    }).toBeInstanceOf(Object);
-  });
-});
