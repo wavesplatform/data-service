125cddf3d16fe6fd3ab35699a90c51baae18bc1e
Аleksej Petrov <apetrov@wavesplatform.com>
2019-05-14 19:28:05 +0300

    delete unnecessery script

--- src/services/state-updates/transformResult.ts
+++
@@ -1,8 +0,0 @@
-import { DataEntryResponse } from '../../protobuf/data-entries';
-import { DataEntryInfo } from '../../types/index';
-import base58 from '../../utils/base58';
-
-export const transformResult = (d: DataEntryResponse): DataEntryInfo => ({
-  ...d,
-  address: base58.encode(d.address),
-});
