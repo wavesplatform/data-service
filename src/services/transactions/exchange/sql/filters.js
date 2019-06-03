c091d9378e90df7ad6398daf9dbe1ee213aef33d
leetwinski <leetwinski@gmail.com>
2019-06-03 18:19:05 +0300

    DATA-945: add orderId filter

--- src/services/transactions/exchange/sql/filters.js
+++ src/services/transactions/exchange/sql/filters.js
@@ -5,10 +5,16 @@ const { where } = require('../../../../utils/db/knex');
 const commonFilters = require('../../_common/sql/filters');
 const commonFiltersOrder = require('../../_common/sql/filtersOrder');
 
-const bySender = curryN(2, (sender, q) =>
-  q
-    .clone()
+const bySender = curryN(
+  2,
+  (sender, q) => q.clone()
     .whereRaw("array[order1->>'sender', order2->>'sender'] @> ?", `{${sender}}`)
+);
+
+const byOrder = curryN(
+  2,
+  (orderId, q) => q.clone()
+    .whereRaw("array[order1->>'id', order2->>'id'] @> ?", `{${orderId}}`)
 );
 
 module.exports = {
@@ -17,6 +23,7 @@ module.exports = {
     matcher: where('t.sender'),
     amountAsset: where('t.amount_asset'),
     priceAsset: where('t.price_asset'),
+    orderId: byOrder,
     sender: bySender,
     sortOuter: s => q =>
       q
@@ -24,5 +31,5 @@ module.exports = {
         .orderBy('time_stamp', s)
         .orderBy('id', s),
   },
-  filtersOrder: [...commonFiltersOrder, 'matcher', 'amountAsset', 'priceAsset'],
+  filtersOrder: [...commonFiltersOrder, 'matcher', 'orderId', 'amountAsset', 'priceAsset'],
 };
