9682f4745501adfc18bb81b164b88f66689042c5
Аleksej Petrov <apetrov@wavesplatform.com>
2019-08-06 18:31:49 +0300

    not found middleware bug fix

--- src/middleware/notFoundHandler.js
+++ src/middleware/notFoundHandler.js
@@ -1,6 +1,8 @@
 const { DEFAULT_NOT_FOUND_MESSAGE } = require('../errorHandling');
 
-module.exports = async ctx => {
+module.exports = async (ctx, next) => {
+  await next();
+  
   if (ctx.status === 404) {
     ctx.status = 404;
     ctx.body = {
