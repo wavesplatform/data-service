f3d1fd9908f872582541a66cdc46c09197166bbb
Аleksej Petrov <apetrov@wavesplatform.com>
2019-08-28 13:10:57 +0300

    fix ParseValueRequest type

--- src/endpoints/transactions/parseDataTxFilters.ts
+++ src/endpoints/transactions/parseDataTxFilters.ts
@@ -9,7 +9,7 @@ type DataType = 'binary' | 'boolean' | 'integer' | 'string';
 
 type ParseValueRequest = {
   type: DataType;
-  value?: any;
+  value?: string;
 };
 
 const parseValue = ({ type, value }: ParseValueRequest) => {
