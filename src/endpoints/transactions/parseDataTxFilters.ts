f58158f41cc188e129adadcc860c7d1fa7ed172c
Аleksej Petrov <apetrov@wavesplatform.com>
2019-08-28 13:06:32 +0300

    add parseDataTxFilters.ts

---
+++ src/endpoints/transactions/parseDataTxFilters.ts
@@ -0,0 +1,55 @@
+import { reject, isNil } from 'ramda';
+import { parseDate } from '../../utils/parseDate';
+import { parseBool } from '../utils/parseBool';
+import { parseArrayQuery } from '../utils/parseArrayQuery';
+
+import { BigNumber } from '@waves/data-entities';
+
+type DataType = 'binary' | 'boolean' | 'integer' | 'string';
+
+type ParseValueRequest = {
+  type: DataType;
+  value?: any;
+};
+
+const parseValue = ({ type, value }: ParseValueRequest) => {
+  if (type === 'boolean') return parseBool(value);
+  else if (type === 'integer' && !isNil(value)) return new BigNumber(value);
+  else return value;
+};
+
+export const parseFilters = ({
+  ids,
+  timeStart, // No default value for timestart, other way - bad for desc pagination
+  timeEnd,
+  sender,
+  limit = '100',
+  sort = 'desc',
+  key,
+  type,
+  value,
+  after,
+}: {
+  ids?: string | string[];
+  timeStart: string;
+  timeEnd: string;
+  sender: string;
+  limit: string;
+  sort: string;
+  key: string;
+  type: DataType;
+  value: string;
+  after: string;
+}) =>
+  reject(isNil, {
+    ids: ids && parseArrayQuery(ids),
+    timeStart: timeStart && parseDate(timeStart).getOrElse(new Date(timeStart)),
+    timeEnd: timeEnd && parseDate(timeEnd).getOrElse(new Date(timeEnd)),
+    limit: parseInt(limit),
+    sort,
+    sender,
+    key,
+    type,
+    value: parseValue({ type, value }),
+    after,
+  });
