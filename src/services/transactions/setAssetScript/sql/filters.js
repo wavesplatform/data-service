e93d1a5ff050e46c0594735f626732024db0d39f
Alexey Petrov <ua.lewka@gmail.com>
2019-09-27 17:48:58 +0300

    DATA-1063 fix script contained txs filtering (#260)
    * fix script contained txs filtering
    
    * generate md5 string for script searching on ds

--- src/services/transactions/setAssetScript/sql/filters.js
+++ src/services/transactions/setAssetScript/sql/filters.js
@@ -1,4 +1,5 @@
-const { where } = require('../../../../utils/db/knex');
+const crypto = require('crypto');
+const { where, whereRaw } = require('../../../../utils/db/knex');
 
 const commonFilters = require('../../_common/sql/filters');
 const commonFiltersOrder = require('../../_common/sql/filtersOrder');
@@ -7,7 +8,14 @@ module.exports = {
   filters: {
     ...commonFilters,
     assetId: where('asset_id'),
-    script: where('script'),
+    script: s =>
+      whereRaw(
+        'md5(script) = ?',
+        crypto
+          .createHash('md5')
+          .update(s)
+          .digest('hex')
+      ),
   },
   filtersOrder: [...commonFiltersOrder, 'assetId', 'script'],
 };
