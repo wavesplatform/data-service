407cc535b9f66650c7f2e3d5077ce4a86f1bab03
Alexey Petrov <ua.lewka@gmail.com>
2019-11-08 16:41:08 +0300

    DATA-1073 fix daemons (#279)
    * fix daemons error reporting; add pg statement timeout
    
    * delete unnecessary params

--- src/daemons/candles/index.js
+++ src/daemons/candles/index.js
@@ -1,21 +1,32 @@
-const createDaemon = require('./create');
+const { loadConfig } = require('./loadConfig');
+const options = loadConfig();
 
 // logger
 const createLogger = require('../../logger/winston');
-
-// pg
-const { createPgDriver } = require('../../db');
-const { loadConfig } = require('./loadConfig');
-const options = loadConfig();
-const pgDriver = createPgDriver(options);
 const logger = createLogger({
   logLevel: 'info',
 });
 
+// pg
+const { createPgDriver } = require('../../db');
+const { withStatementTimeout } = require('../../db/driver');
+const pgDriver = withStatementTimeout(
+  createPgDriver(options),
+  options.candlesUpdateTimeout,
+  0 // 0 - unset statement timeout
+);
+
 const { daemon: runDaemon } = require('../presets/daemon');
+const createDaemon = require('./create');
 
 runDaemon(
-  createDaemon({ logger: logger, pg: pgDriver }, options),
+  createDaemon(
+    {
+      logger: logger,
+      pg: pgDriver,
+    },
+    options
+  ),
   options,
   options.candlesUpdateInterval,
   options.candlesUpdateTimeout,
