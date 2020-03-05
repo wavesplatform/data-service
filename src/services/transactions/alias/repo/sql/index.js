5f1339a86ab0e91a2dfd2942894d708d5e397eda
Аleksej Petrov <apetrov@wavesplatform.com>
2020-03-05 20:31:36 +0300

    sql performance tuning: bug fixes

--- src/services/transactions/alias/repo/sql/index.js
+++ src/services/transactions/alias/repo/sql/index.js
@@ -1,4 +1,4 @@
-const { compose, defaultTo } = require('ramda');
+const { compose } = require('ramda');
 
 const { createSql } = require('../../../_common/sql');
 const { sort } = require('../../../_common/sql/filters');
@@ -7,12 +7,9 @@ const { SORT } = require('../../../_common/sql/defaults');
 const { select, selectFromFiltered } = require('./query');
 const { filters, filtersOrder } = require('./filters');
 
-const sortWithDefault = defaultTo(SORT);
-
 const queryAfterFilters = {
-  // get, mget does not has sort value
-  get: (q, fValues) => selectFromFiltered(sortWithDefault(fValues.sort))(q),
-  mget: (q, fValues) => selectFromFiltered(sortWithDefault(fValues.sort))(q),
+  get: selectFromFiltered(SORT),
+  mget: selectFromFiltered(SORT),
   search: (q, fValues) =>
     compose(
       // outer sort
