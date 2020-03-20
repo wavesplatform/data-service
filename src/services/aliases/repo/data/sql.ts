4a7237e982d3c49ade166c0e3200778b2dc8067d
Аleksej Petrov <apetrov@wavesplatform.com>
2020-03-20 17:40:15 +0300

    fix aliases paginating

--- src/services/aliases/repo/data/sql.ts
+++ src/services/aliases/repo/data/sql.ts
@@ -4,43 +4,58 @@ import { AliasesSearchRequest } from '../../repo';
 
 const pg = knex({ client: 'pg' });
 
+const columns = ['alias', 'address', 'duplicates'];
+const columnsWithRowNumber = [...columns, 'rn'];
+
+const getAliasRowNumber = (after: string) =>
+  pg('aliases_cte')
+    .select('rn')
+    .where('alias', after);
+
 const selectFromSet = (aliasSet: string[] | knex.QueryBuilder) =>
-  pg.select('alias', 'address', 'duplicates').from({
+  pg.select(columnsWithRowNumber).from({
     counted_aliases: pg({ t: 'txs_10' })
       .select({ alias: 't.alias' })
       .min({ address: 'addr.address' }) // first sender
       .min({ time_stamp: 'txs.time_stamp' }) // first tx timestamp
       .count({ duplicates: 't.sender_uid' }) // count senders grouped by alias
+      .column({ rn: pg.raw('row_number() over (order by t.tx_uid)') }) // rn for pagination
       .leftJoin({ txs: 'txs' }, 'txs.uid', 't.tx_uid')
       .leftJoin({ addr: 'addresses' }, 'addr.uid', 't.sender_uid')
       .whereIn('t.alias', aliasSet)
-      .groupBy('t.alias'),
+      .groupBy('t.alias', 't.tx_uid'),
   });
 
 export default {
   get: (alias: string) =>
     selectFromSet([alias])
+      .select(columns)
       .clone()
       .toString(),
   mget: (aliases: string[]) =>
     selectFromSet(aliases)
+      .select(columns)
       .clone()
       .toString(),
   search: ({ address, showBroken, after, limit }: AliasesSearchRequest) => {
-    const q = selectFromSet(
-      pg('txs_10')
-        .select('alias')
-        .where(
-          'sender_uid',
-          pg('addresses')
-            .select('uid')
-            .from('addresses')
-            .where('address', address)
-            .limit(1)
+    const q = pg('aliases_cte')
+      .with(
+        'aliases_cte',
+        selectFromSet(
+          pg('txs_10')
+            .select('alias')
+            .where(
+              'sender_uid',
+              pg('addresses')
+                .select('uid')
+                .from('addresses')
+                .where('address', address)
+                .limit(1)
+            )
         )
-    )
-      .clone()
-      .orderBy('time_stamp', 'asc')
+      )
+      .select(columns)
+      .orderBy('rn', 'asc')
       .limit(limit);
 
     return compose<knex.QueryBuilder, knex.QueryBuilder, string>(
@@ -52,7 +67,7 @@ export default {
               .clone()
               .where('duplicates', 1)
               .toString(),
-      q => (after ? q.where('alias', '<', after) : q)
+      q => (after ? q.where('rn', '>', getAliasRowNumber(after)) : q)
     )(q);
   },
 };
