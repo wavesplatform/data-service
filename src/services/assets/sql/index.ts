1493d92519afd45e7bbc01709062f07cc5df2143
Аleksej Petrov <apetrov@wavesplatform.com>
2021-03-03 17:51:37 +0300

    add pagination

--- src/services/assets/sql/index.ts
+++ src/services/assets/sql/index.ts
@@ -1,21 +1,17 @@
 import * as knex from 'knex';
 import { compose, isNil } from 'ramda';
+import { AssetsSearchRequest, SearchByTicker } from '../types';
 import { columns } from './common';
 import { searchAssets } from './searchAssets';
-import { AssetsSearchRequest, SearchByTicker } from '../types';
+import { searchAssetsByTicker } from './searchAssetsByTicker';
 
 const pg = knex({ client: 'pg' });
 
 const getAssetIndex = (asset_id: string) =>
-  pg('assets_cte')
-    .column('rn')
-    .where('asset_id', asset_id);
+  pg('assets_cte').column('rn').where('asset_id', asset_id);
 
 export const mget = (ids: string[]): string =>
-  pg('assets')
-    .select(columns)
-    .whereIn('asset_id', ids)
-    .toString();
+  pg('assets').select(columns).whereIn('asset_id', ids).toString();
 
 export const get = (id: string): string => mget([id]);
 
@@ -28,10 +24,8 @@ export const search = (req: AssetsSearchRequest): string => {
       (q: knex.QueryBuilder) => q.toString(),
       (q: knex.QueryBuilder) => q.clone().limit(req.limit),
       (q: knex.QueryBuilder) =>
-        req.ticker === '*'
-          ? q.whereNotNull('ticker')
-          : q.where('ticker', req.ticker)
-    )(pg('assets').select(columns));
+        req.after ? q.clone().where('rn', '>', getAssetIndex(req.after)) : q
+    )(searchAssetsByTicker(req.ticker));
   } else {
     return compose(
       (q: knex.QueryBuilder) => q.toString(),
