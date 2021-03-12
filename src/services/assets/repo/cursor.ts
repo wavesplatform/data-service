b0dfaf8d2f86ddec164ce1142ea0234a86e40e74
Аleksej Petrov <apetrov@wavesplatform.com>
2021-03-12 17:06:31 +0300

    fix assets service pagination cursor

--- src/services/assets/repo/cursor.ts
+++ src/services/assets/repo/cursor.ts
@@ -2,7 +2,7 @@ import { Result, Ok as ok } from 'folktale/result';
 import { ValidationError } from '../../../errorHandling';
 import { AssetsSearchRequest, AssetDbResponse } from './types';
 
-export type Cursor = number;
+export type Cursor = string;
 
 export const serialize = <Response extends AssetDbResponse>(
   request: AssetsSearchRequest,
@@ -12,10 +12,5 @@ export const serialize = <Response extends AssetDbResponse>(
     ? undefined
     : Buffer.from(response.asset_id.toString()).toString('base64');
 
-export const deserialize = (
-  cursor: string
-): Result<ValidationError, Cursor> => {
-  return ok<ValidationError, Cursor>(
-    parseInt(Buffer.from(cursor, 'base64').toString('utf-8'))
-  );
-};
+export const deserialize = (cursor: string): Result<ValidationError, Cursor> =>
+  ok<ValidationError, Cursor>(Buffer.from(cursor, 'base64').toString('utf-8'));
