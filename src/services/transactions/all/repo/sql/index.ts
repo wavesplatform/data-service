5cf3ad06ee616c2f0ad36bd127aa7881c6e282a8
Аleksej Petrov <apetrov@wavesplatform.com>
2021-03-24 21:40:31 +0300

    fix all txs fetching performance

--- src/services/transactions/all/repo/sql/index.ts
+++ src/services/transactions/all/repo/sql/index.ts
@@ -1,9 +1,16 @@
 import { createSql } from '../../../_common/sql';
 
-import { select } from './query';
+import { select, selectFromFiltered } from './query';
 import { filters } from './filters';
+
+const queryAfterFilters = {
+  get: selectFromFiltered,
+  mget: selectFromFiltered,
+  search: selectFromFiltered,
+};
 
 export default createSql({
   query: select,
+  queryAfterFilters,
   filters,
 });
--- src/services/transactions/all/repo/sql/query.ts
+++ src/services/transactions/all/repo/sql/query.ts
@@ -1,9 +1,21 @@
 import * as knex from 'knex';
 const pg = knex({ client: 'pg' });
 
-export const select = pg({ t: 'txs' }).select({
-  uid: 't.uid',
-  tx_type: 't.tx_type',
-  id: 't.id',
-  time_stamp: 't.time_stamp',
-});
+export const select = pg({ t: 'txs' }).select('uid');
+
+export const selectFromFiltered = (filtered: knex.QueryBuilder) =>
+  pg
+    .with('ts', filtered)
+    .select({
+      uid: 't.uid',
+      tx_type: 't.tx_type',
+      id: 't.id',
+      time_stamp: 't.time_stamp',
+    })
+    .from('ts')
+    .join(
+      { t: 'txs' },
+      {
+        't.uid': 'ts.uid',
+      }
+    );
